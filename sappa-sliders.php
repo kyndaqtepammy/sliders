dca931f8016b0f35a437100e794d418bb07553f4
kyndaqtepammy <pamsillah@yahoo.co.uk>
2021-02-27 17:46:58 +0200

    Added jquerydepency to slick enqueue to prevent "jquery not defined" error

--- sappa-sliders.php
+++ sappa-sliders.php
@@ -8,16 +8,17 @@
 function admin_scripts() {
   wp_enqueue_style('sapcss', plugins_url( '/css/slick.css' , __FILE__ ));
   wp_enqueue_style('saptcss', plugins_url( '/css/slick-theme.css' , __FILE__ ));
-  wp_enqueue_script('sapjs', plugins_url( '/js/slick.js' , __FILE__ ), 'jquery', true);
+  wp_enqueue_script('sapjs', plugins_url( '/js/slick.js' , __FILE__ ), array('jquery'), true);
 }
 add_action( 'wp_enqueue_scripts', 'admin_scripts' );
 
 function vertical_slider() { ?>
-  <div class="my-carousel">
+  <div class="sappa-carousel">
     <div><img src="<?php echo plugins_url('img/what-we-do.jpg', __FILE__ ) ?>"></div>
     <div><img src="<?php echo plugins_url('img/where-we-are.jpg', __FILE__ ) ?>"></div>
     <div><img src="<?php echo plugins_url('img/who-we-are.jpg', __FILE__ ) ?>"></div>
 </div>
 <?php  
 }
+add_shortcode('slider-1', 'vertical_slider');
  ?>
